74ae367c316b15d96c20066f1198a9066c27d626
ixio0330 <ixio0330@gmail.com>
2022-07-05 05:44:24 +0900

    [Feat] User create, update

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -2,17 +2,32 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
+import { getTodayTimestamp } from '../helper/timestamp';
 
 @Injectable()
 export class UsersService {
   private users: User[] = [];
+  private id = 0;
 
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  create(createUserDto: CreateUserDto): User {
+    const user: User = {
+      ...createUserDto,
+      id: this.id,
+      created_on: getTodayTimestamp(),
+    };
+    this.users.push(user);
+    return user;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  update(id: number, updateUserDto: UpdateUserDto): User {
+    const updateUser = this.findOne(id);
+    const user = {
+      ...updateUser,
+      ...updateUserDto,
+      updated_on: getTodayTimestamp(),
+    };
+    this.users.push(user);
+    return user;
   }
 
   remove(id: number): User[] {
